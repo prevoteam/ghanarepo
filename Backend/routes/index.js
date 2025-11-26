b3f74624e910bfb0d9a3aab3cf971390b521af35
Chaitanya19-Dev <lichade012@gmail.com>
2025-11-26 12:00:52 +0530

    Added changes in routes/index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const homeController = require('../controller/homeController');
+const monitoringController = require('../controller/monitoringController');
+const pspController = require('../controller/pspController');
 
 
 const routerInitialize = (config) => {
     const router = express.Router();
 
- 
+
     router.use('/home', homeController({ config }));
+    router.use('/admin/monitoring', monitoringController({ config }));
+    router.use('/home/psp', pspController({ config }));
 
     return router;
 }
